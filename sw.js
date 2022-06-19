ec07e6c4a9ef7e9c3544d517796635bcb0a483e4
NonyeReeta <nonyeibeanu@gmail.com>
2022-06-19 20:13:18 +0200

    add caching for resources

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+self.addEventListener("install", function (event) {
+  const urlsToCache = [
+    "/",
+    "/restaurant.html",
+    "js/main.js",
+    "js/restaurant_info.js",
+    "css/styles.css",
+    "img/",
+    "data/restaurants.json",
+  ];
+  event.waitUntil(
+    caches.open("restaurant-static").then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
 self.addEventListener("fetch", function (event) {
-  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
 });
